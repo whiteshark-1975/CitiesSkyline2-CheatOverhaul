39b44bddc9e853744b8e85ab90be65637510c2e5
Yves De Wit <yvesdewit1975@gmail.com>
2023-12-12 15:51:44 +0100

    Corrected float

--- Options/SignatureMixedbuildingOptions.cs
+++ Options/SignatureMixedbuildingOptions.cs
@@ -5,9 +5,9 @@ public class SignatureMixedbuildingOptions
     public string Name { get; set; } = null!;
 
     public int Attractiveness { get; set; }
-    public int Wellbeing { get; set; }
-    public int Radius { get; set; }
-    public int CityAttractiveness { get; set; }
+    public float Wellbeing { get; set; }
+    public float Radius { get; set; }
+    public float CityAttractiveness { get; set; }
     public int Noisepollution { get; set; }
 
 }
