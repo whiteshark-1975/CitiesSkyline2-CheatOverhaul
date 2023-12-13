5c7fe367f383ada8ea587fb85f14f5372bf009c4
Yves De Wit <yvesdewit1975@gmail.com>
2023-12-13 20:43:10 +0100

    cor0.1

--- Options/SignatureOfficebuildingOptions.cs
+++ Options/SignatureOfficebuildingOptions.cs
@@ -4,14 +4,13 @@ public class SignatureOfficebuildingOptions
 {
     public string Name { get; set; } = null!;
 
-    public int Attractiveness { get; set; }
-    public float Wellbeing { get; set; }
-    public float Radius { get; set; }
-    public float CrimeAccumulation { get; set; }
-    public float CrimeRadius { get; set; }
-    public float CityOfficeEfficency { get; set; }
-    public float CityUniversityInterest { get; set; }
-    public float CityUniversityGraduation { get; set; }
-    public int Noisepollution { get; set; }
-
+    public int Attractiveness { get; set; } = 0
+    public float Wellbeing { get; set; } = 1f
+    public float Radius { get; set; } = 1f
+    public float CrimeAccumulation { get; set; } = 0f
+    public float CrimeRadius { get; set; } = 0f
+    public float CityOfficeEfficency { get; set; } = 1f
+    public float CityUniversityInterest { get; set; } = 1f
+    public float CityUniversityGraduation { get; set; } = 1f
+    public int Noisepollution { get; set; } = 1f
 }
