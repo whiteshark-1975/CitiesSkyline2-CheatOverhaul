1c26372080fe7660591af76c0ced80c70e92fe42
whiteshark-1975 <yvesdewit1975@gmail.com>
2023-12-23 10:28:04 +0100

    added localeffect

---
+++ Interfaces/ICityEffectOptions.cs
@@ -0,0 +1,30 @@
+﻿public interface ICityEffectOptions
+
+{
+    public float CityAttractiveness { get; set; }
+    public float CityDisasterDamageRate { get; set; }
+    public float CityDisasterWarningTime { get; set; }
+    public float CityDiseaseprobability { get; set; }
+    public float CityEntertainment { get; set; }
+    public float CityExportCost { get; set; }
+    public float CityHospitalEfficiency { get; set; }
+    public float CityImportcost { get; set; }
+    public float CityIndustrialEfficiency { get; set; }
+    public float CityIndustrialElectronicDemand { get; set; }
+    public float CityIndustrialElectronicEfficiency { get; set; }
+    public float CityIndustrialGroundPollution { get; set; }
+    public float CityLoanIntrest { get; set; }
+    public float CityOfficeEfficiency { get; set; }
+    public float CityOfficeSoftwareDemand { get; set; }
+    public float CityOfficeSoftwareEfficiency { get; set; }
+    public float CityOilResourceAmmount { get; set; }
+    public float CityOreResourceAmmount { get; set; }
+    public float CityParkEntertainment { get; set; }
+    public float CityPollutionhealthAffect { get; set; }
+    public float CityRecoverFailChange { get; set; }
+    public float CityTelecomCapacity { get; set; }
+    public float CityUniversityGraduation { get; set; }
+    public float CityUniversityInterest { get; set; }
+
+
+}
---
+++ Interfaces/ILocalEffectsOptions.cs
@@ -0,0 +1,14 @@
+﻿public interface ILocalEffectsOptions
+
+{
+    public float LocalWellbeing { get; set; }
+    public float LocalWellbeingRadius { get; set; }
+    public float LocalCrimeAccumulation { get; set; }
+    public float LocalCrimeAccumulationRadius { get; set; }
+    public float LocalForestFireHazard { get; set; }
+    public float LocalForestFireHazardRadius { get; set; }
+    public float LocalForestFireResponseTime { get; set; }
+    public float LocalForestFireResponseTimeRadius { get; set; }
+
+}
+
